622fd26de41c3eaa20266eea97dde58c53980b11
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-03-26 16:18:17 -0500

    merge

--- week-2/berryhill-payroll.js
+++ week-2/berryhill-payroll.js
@@ -7,20 +7,122 @@
 ;==========================================
 */
 
+/*
+==========================================
+;Assigns list item 1 within the first unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_oneOne = document.getElementById("oneOne").innerHTML = "Ludwig";
+
+/*
+==========================================
+;Assigns list item 2 within the first unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_oneTwo = document.getElementById("oneTwo").innerHTML = "Beethoven";
+
+/*
+==========================================
+;Assigns list item 3 within the first unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_oneThree = document.getElementById("oneThree").innerHTML = "505 main street";
+
+/*
+==========================================
+;Assigns list item 4 within the first unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_oneFour = document.getElementById("oneFour").innerHTML = new Date().toLocaleDateString('en-US');
-const li_oneFive = document.getElementById("oneFive").innerHTML = 25.501.toFixed(1);
+
+/*
+==========================================
+;Assigns list item 5 within the first unordered list of the HTML to a string value
+;=========================================
+*/
+
+const li_oneFive = document.getElementById("oneFive").innerHTML = 18.9.toFixed(1);
+
+/*
+==========================================
+;Assigns list item 1 within the second unordered list of the HTML to a string value
+;=========================================
+*/
 
 const li_twoOne = document.getElementById("twoOne").innerHTML = "Johann";
+
+/*
+==========================================
+;Assigns list item 2 within the second unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_twoTwo = document.getElementById("twoTwo").innerHTML = "Bach";
+
+/*
+==========================================
+;Assigns list item 3 within the second unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_twoThree = document.getElementById("twoThree").innerHTML = "512 main street";
+
+/*
+==========================================
+;Assigns list item 4 within the second unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_twoFour = document.getElementById("twoFour").innerHTML = new Date().toLocaleDateString('en-US');
+
+/*
+==========================================
+;Assigns list item 5 within the second unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_twoFive = document.getElementById("twoFive").innerHTML = 25.501.toFixed(1);
 
+/*
+==========================================
+;Assigns list item 1 within the third unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_threeOne = document.getElementById("threeOne").innerHTML = "Wolfgang";
+
+/*
+==========================================
+;Assigns list item 2 within the third unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_threeTwo = document.getElementById("threeTwo").innerHTML = "Mozart";
+
+/*
+==========================================
+;Assigns list item 3 within the third unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_threeThree = document.getElementById("threeThree").innerHTML = "600 main street";
+
+/*
+==========================================
+;Assigns list item 4 within the third unordered list of the HTML to a string value
+;=========================================
+*/
+
 const li_threeFour = document.getElementById("threeFour").innerHTML = new Date().toLocaleDateString('en-US');
-const li_threeFive = document.getElementById("threeFive").innerHTML = 25.501.toFixed(1);
+
+/*
+==========================================
+;Assigns list item 5 within the third unordered list of the HTML to a string value
+;=========================================
+*/
+
+const li_threeFive = document.getElementById("threeFive").innerHTML = 50.1.toFixed(1);
